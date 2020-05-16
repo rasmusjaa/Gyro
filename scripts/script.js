15edc6c427356d0015f894b8c114a3c67dede008
rasmusjaa <rasmus@lense.fi>
2020-05-16 16:06:23 +0300

    added football following x and y coordinates clamped to -29 - 29 degrees

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ var rot_z = 0;
 var acc_x = 0;
 var acc_y = 0;
 var acc_z = 0;
+var ball_x = 0;
+var ball_y = 0;
 var looping = true;
 var cookie = "";
 
@@ -14,21 +16,29 @@ $(document).ready(function() {
 
 	window.addEventListener('deviceorientation', function( event ) {
 		deviceOrientationData = event;
-		// X -180 - 180
-		rot_x = deviceOrientationData.beta.toFixed(2);
-		// X -90 - 90, loops twice over
-		rot_y = deviceOrientationData.gamma.toFixed(2);
-		// Z -180 - 180
-		rot_z = deviceOrientationData.alpha.toFixed(2);
-		
+		if (event)
+		{
+			// X -180 - 180
+			rot_x = deviceOrientationData.beta.toFixed(2);
+			// X -90 - 90, loops twice over
+			rot_y = deviceOrientationData.gamma.toFixed(2);
+			// Z -180 - 180
+			rot_z = deviceOrientationData.alpha.toFixed(2);
+		}
 	}, false);
 
 
 	function handleMotionEvent(event) {
-
-		acc_x = event.accelerationIncludingGravity.x.toFixed(2);
-		acc_y = event.accelerationIncludingGravity.y.toFixed(2);
-		acc_z = event.accelerationIncludingGravity.z.toFixed(2);
+		if (event && event.accelerationIncludingGravity
+			&& event.accelerationIncludingGravity.x
+			&& event.accelerationIncludingGravity.y
+			&& event.accelerationIncludingGravity.z)
+		{
+			// with gravity 9,807 m/s²
+			acc_x = event.accelerationIncludingGravity.x.toFixed(2);
+			acc_y = event.accelerationIncludingGravity.y.toFixed(2);
+			acc_z = event.accelerationIncludingGravity.z.toFixed(2);
+		}
 	}
 	
 	window.addEventListener("devicemotion", handleMotionEvent, true);
@@ -42,29 +52,40 @@ $(document).ready(function() {
 
 
 	window.setInterval(function(){
-		$("#data").text('Orientation');
-		$("#data").append(`<br>Rotation X ${rot_x}`);
-		$("#data").append(`<br>Rotation Y ${rot_y}`);
-		$("#data").append(`<br>Rotation Z ${rot_z}`);
-		$("#data").append(`<br>Acceleration X ${acc_x}`);
-		$("#data").append(`<br>Acceleration Y ${acc_y}`);
-		$("#data").append(`<br>Acceleration Z ${acc_z}`);
-		$("#data").append(`<br>screen rotated ${currentScreenOrientation} degrees`);
-	  }, 10);
+		if (looping)
+		{
+			$("#data").text('Orientation');
+			$("#data").append(`<br>Rotation X ${rot_x}`);
+			$("#data").append(`<br>Rotation Y ${rot_y}`);
+			$("#data").append(`<br>Rotation Z ${rot_z}`);
+			$("#data").append(`<br>Acceleration X ${acc_x}`);
+			$("#data").append(`<br>Acceleration Y ${acc_y}`);
+			$("#data").append(`<br>Acceleration Z ${acc_z}`);
+			$("#data").append(`<br>screen rotated ${currentScreenOrientation} degrees`);
+			
+			var ball = document.getElementById('ball');
+			ball_x = clamp(rot_x, -29, 29) * 5 + 145;
+			ball_y = clamp(rot_y, -29, 29) * 5 + 145;
+			ball.style.left = ball_x+'px';
+			ball.style.top = ball_y+'px';
+		}
+	}, 10);
 	
 
 	$('#loop').click(function() {
 		if (looping) {
 			looping = false;
-			clearInterval();
 		}
 		else {
 			looping = true;
-			window.setInterval();
 		}
 	});
 
 });
+
+function clamp(num, min, max) {
+	return num <= min ? min : num >= max ? max : num;
+}
 
 window.onunload = function () {
 	if ($("#remember").is(':checked')) {
